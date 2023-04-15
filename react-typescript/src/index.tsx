9c15dc566a516685194be3df5deee1d2f8974d92
Aaron Crane <skilledjuggler@gmail.com>
2023-04-15 10:44:49 -0700

    added VisuallyHidden component

--- react-typescript/src/index.tsx
+++ react-typescript/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import GlobalStyles from './GlobalStyles';
 
-const root = ReactDOM.createRoot(
+const root = createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
